760ff01be4b81b85a0bad06321bf3aa7b096a96b
jamespenghong <812117714@qq.com>
2018-02-11 16:43:27 +0800

    v0.1

--- djangogirl/settings.py
+++ djangogirl/settings.py
@@ -118,6 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),    #设置一下静态文件文件夹
+]
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
